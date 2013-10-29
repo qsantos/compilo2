c590ff1b1d988e2e92b1d5ec0243d5ed9cdd50ec
Quentin SANTOS <quentin.santos@worldonline.fr>
2013-10-29 11:05:42 +0100

    Added constructors for AST expressions

--- ast.c
+++ ast.c
@@ -1,1 +1,45 @@
 #include "ast.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void* smalloc(size_t size, const char* file, int line)
+{
+	void* ret = malloc(size);
+	if (!ret)
+	{
+		fprintf(stderr, "Allocation failed at %s:%i\n", file, line);
+		exit(1);
+	}
+	return ret;
+}
+
+#define MALLOC(T) ((T*) smalloc(sizeof(T), __FILE__, __LINE__))
+
+#define EXPR_UNI(N,C) \
+ast_expr_t* expr_##N(ast_expr_t* a) \
+{ \
+	ast_expr_t* ret = MALLOC(ast_expr_t); \
+	ret->type = C; \
+	ret->v.uni.a = a; \
+	return ret; \
+} \
+
+EXPR_UNI(inc, E_INC)
+EXPR_UNI(dec, E_DEC)
+
+#define EXPR_BIN(N,C) \
+ast_expr_t* expr_##N(ast_expr_t* a, ast_expr_t* b) \
+{ \
+	ast_expr_t* ret = MALLOC(ast_expr_t); \
+	ret->type = C; \
+	ret->v.bin.a = a; \
+	ret->v.bin.b = b; \
+	return ret; \
+} \
+
+EXPR_BIN(add, E_ADD)
+EXPR_BIN(sub, E_SUB)
+EXPR_BIN(mul, E_MUL)
+EXPR_BIN(div, E_DIV)
+EXPR_BIN(mod, E_MOD)
--- ast.h
+++ ast.h
@@ -14,12 +14,21 @@ struct ast_expr_t
 		E_MUL,
 		E_DIV,
 		E_MOD,
-	} type_t;
+	} type;
 	union
 	{
 		struct { ast_expr_t *a;    } uni;
 		struct { ast_expr_t *a,*b; } bin;
 	} v;
 };
+
+ast_expr_t* expr_inc(ast_expr_t* a);
+ast_expr_t* expr_dec(ast_expr_t* a);
+
+ast_expr_t* expr_add(ast_expr_t* a, ast_expr_t* b);
+ast_expr_t* expr_sub(ast_expr_t* a, ast_expr_t* b);
+ast_expr_t* expr_mul(ast_expr_t* a, ast_expr_t* b);
+ast_expr_t* expr_div(ast_expr_t* a, ast_expr_t* b);
+ast_expr_t* expr_mod(ast_expr_t* a, ast_expr_t* b);
 
 #endif
