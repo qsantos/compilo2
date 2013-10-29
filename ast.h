6641fd6919b1edc3c15fccee89cdf2bacb91da4d
Quentin SANTOS <quentin.santos@worldonline.fr>
2013-10-29 10:52:53 +0100

    Creating basic type structure for the AST

--- ast.h
+++ ast.h
@@ -1,4 +1,25 @@
 #ifndef AST_H
 #define AST_H
 
+typedef struct ast_expr_t ast_expr_t;
+
+struct ast_expr_t
+{
+	enum
+	{
+		E_INC,
+		E_DEC,
+		E_ADD,
+		E_SUB,
+		E_MUL,
+		E_DIV,
+		E_MOD,
+	} type_t;
+	union
+	{
+		struct { ast_expr_t a;   } uni;
+		struct { ast_expr_t a,b; } bin;
+	} v;
+};
+
 #endif
