3642de9404b5ae8fd0716a26573a393d901a1a88
Quentin SANTOS <quentin.santos@worldonline.fr>
2013-10-30 09:51:57 +0100

    Added (re)allocation macros
    Defined error handling function srealloc() and macros MMALLOC (array
    allocation), REALLOC (reallocation) and MREALLOC (array reallocation).

--- util.c
+++ util.c
@@ -8,7 +8,18 @@ void* smalloc(size_t size, const char* file, int line)
 	void* ret = malloc(size);
 	if (!ret)
 	{
-		fprintf(stderr, "Allocation failed at %s:%i\n", file, line);
+		fprintf(stderr, "Allocation of %zu bytes failed at %s:%i\n", size, file, line);
+		exit(1);
+	}
+	return ret;
+}
+
+void* srealloc(void* ptr, size_t size, const char* file, int line)
+{
+	void* ret = realloc(ptr, size);
+	if (!ret)
+	{
+		fprintf(stderr, "Reallocation of %zu bytes failed at %s:%i\n", size, file, line);
 		exit(1);
 	}
 	return ret;
